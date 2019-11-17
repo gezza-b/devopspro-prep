7e067443475e9c3c793749c3faf9c8411306c1c0
gezza-b <bachlmayr@gmail.com>
2019-11-17 15:56:24 +1100

    starting with Rekognition

--- lambda/imghandler.go
+++ lambda/imghandler.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
-	// "github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
 )
 
 const resp string = "Hello"
+const bucket string = "imghandler-gez"
+const region string = "us-east-1"
+const imgName string = "2faces.jpeg"
 
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
+// https://docs.aws.amazon.com/lambda/latest/dg/go-programming-model-context.html
+// func HandleRequest(ctx context.Context, name MyEvent) (response string, err error) {
 func HandleRequest(name MyEvent) (response string, err error) {
-	// xray.Configure(xray.Config{
-	// 	LogLevel:       "info", // default
-	// 	ServiceVersion: "1.2.3",
-	// })
+	// fmt.Println("::ctx: ", ctx)
+	// lc, _ := lambdacontext.FromContext(ctx)
+	// reqId := lc.AwsRequestID
+	// fmt.Println(":reqID: ", reqId)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
 
-	//return fmt.Sprintf("Hello %s!", name.Name), nil
+	if err != nil {
+		fmt.Println("NewSession error: ", err)
+		return "SendMsg error while creating session", err
+	}
+
+	client := rekognition.New(sess)
+
+	input := &rekognition.DetectLabelsInput{
+		Image: &rekognition.Image{
+			S3Object: &rekognition.S3Object{
+				Bucket: aws.String(bucket),
+				Name:   aws.String(imgName),
+			},
+		},
+		MaxLabels:     aws.Int64(100),
+		MinConfidence: aws.Float64(70.000000),
+	}
+	result, err := client.DetectLabels(input)
+
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		//https://blog.golang.org/json-and-go
+		var f interface{}
+		err := json.Unmarshal(b, &f)
+
+		fmt.Println(":result: ", result)
+	}
 	return resp, nil
 }
 
--- lambda/imghandler_test.go
+++ lambda/imghandler_test.go
@@ -17,6 +17,7 @@ func TestUploadEvent(t *testing.T) {
 	data := MyEvent{}
 
 	_ = json.Unmarshal([]byte(file), &data)
+	// var res, err = HandleRequest(context.TODO(), data)
 	var res, err = HandleRequest(data)
 	assert.Equal(t, err, nil)
 	assert.Equal(t, res, expectedResp)
