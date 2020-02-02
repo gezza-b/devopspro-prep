37c73f93f5403c046dd16f10dbdf5c869c92c92a
gezza-b <bachlmayr@gmail.com>
2020-02-02 17:10:27 +1100

    new test

--- lambda/getdata/imghandler_test.go
+++ lambda/getdata/imghandler_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const expectedResp = "Hello"
+var expectedLength = len("9476cd6b-7f9e-53b6-9942-9b1232f02810")
+
 const putObjectEvent = "../../testevents/s3upload.json"
 
 func TestUploadEvent(t *testing.T) {
@@ -21,5 +22,5 @@ func TestUploadEvent(t *testing.T) {
 	// var res, err = HandleRequest(context.TODO(), data)
 	var res, err = HandleRequest(nil, data)
 	assert.Equal(t, err, nil)
-	assert.Equal(t, res, expectedResp)
+	assert.Equal(t, len(res), expectedLength)
 }
